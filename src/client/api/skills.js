bc87abc56603e2cef1f2d65c50c87f2368f9b80b
raghavmac <rghv.rocking@gmail.com>
2018-12-24 02:12:39 +0530

    Added routes for skills endpoints

--- src/client/api/skills.js
+++ src/client/api/skills.js
@@ -3,10 +3,19 @@ import axios from 'axios';
 class SkillsApi {
 
   static async getAllSkills() {
-    const skills = await axios.get('/skills');
+    const { data: skills } = await axios.get('/skills');
     return skills;
   }
 
+  static async createSkill(data) {
+    const { data: skill } = await axios.post('/skills', data);
+    return skill;
+  }
+
+  static async deleteSkill(id) {
+    const { data } = await axios.delete(`/skills/${id}`);
+    return data;
+  }
 }
 
 export default SkillsApi;
--- src/server/routes/skills.js
+++ src/server/routes/skills.js
@@ -7,6 +7,11 @@ const router = express.Router();
 const uri = `${config.apiService}/api/skills`;
 
 router.route('/')
-  .get((req, res) => request.get(uri).pipe(res));
+  .get((req, res) => request.get(uri).pipe(res))
+
+  .post((req, res) => request.post(uri, { json: req.body }).pipe(res));
+
+router.route('/:id')
+  .delete((req, res) => request.delete(`${uri}/${req.params.id}`).pipe(res));
 
 export default router;
